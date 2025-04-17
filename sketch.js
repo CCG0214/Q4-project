f61d55cf2c0d886f869676e8a45da38f5ce1393e
CCG0214 <cgu25@belmontschools.net>
2025-04-17 09:54:38 -0400

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,99 +1,59 @@
-const paddleSpeed=5;
-const ballSpeed=5;
-var computerPaddleSpeed=1;
+var xBall=Math.floor(Math.random()*300)+100;
+var yBall=100;
+var xSpeed=(2,7);
+var ySpeed=(7,2);
+var score=0
 
-var userPaddle=createPaddle(50,'left');
-var computerPaddle=createPaddle(50,'right');
-var ball=createBall();
-
-var scoreUser=0;
-var scoreComputer=0;
-
-function loop()
-{
-  clear();
-  
-  drawScene();
-  
-  readKeys();
-  updateComputerPaddle();
-  drawPaddles();
-  
-  updateBall();
-  drawBall();
-  
-  checkCollision();
-  
-  checkOut();
-  
-  displayStats();
-  displayInstructions();
+function setup(){
+  createCanvas(400,400);
 }
 
-function checkOut()
-{
-  if (ball.x<0)
-    {
-      scoreComputer++;
-      resetGame();
-    }
-  else if(ball.x>width)
-    {
-      scoreUser++;
-      
-      
-      
-      computerPaddleSpeed+=random(0.75);
-      
-      resetGame();
-    }
+function draw(){
+  
+  background(0);
+  
+  fill('#FFF');
+  rect(mouseX,375,90,15);
+  
+  move();
+  display();
+  bounce();
+  paddle();
+  
+  fill('pink');
+  textSize(24);
+  text("Score:"+score,10,25);
 }
 
-function updateComputerPaddle()
-{
-  if(ball.dx==1)
-    {
-      
-      var y=ball.slope*(computerPaddle.x-ball.x1)+ball.y1;
-      
-      
-      var dy=(y-computerPaddle.y-comuterPaddle.w/2);
-      
-      
-      dy=dy/((computerPaddle.x-ball.x)/ballSpeed);
-      
-      
-      dy=constrain(dy,-computerPaddleSpeed.computerPaddleSpeed);
-      
-      computerPaddle.y+=dy;
-    }
+function move(){
+  xBall+=xSpeed;
+  yBall+=ySpeed;
 }
 
-
-function updateBall()
-{
-  if(!ball.inMotion)
-    return;
-    
-    ball.x+=ball.dx*ballSpeed;
-  ball.y=bal.slope*(ball.x-ball.x1)+ball.y1;
-}
-
-function checkCollision()
-{
-  hitBall(userPaddle);
-  hitBall(computerPaddle);
+function bounce(){
+  if(xBall<10||
+    xBall>400-10){
+    xSpeed*=-1;
   }
+  if(yBall<10||
+    yBall>400-10){
+    ySpeed=-1;
+  }
+}
 
-function hitBall(paddle)
-{
-  if(collisionCircleRect(ball.x,ball.y,ball.r,paddle.x,paddle.y,paddle.h,paddle.w))
-    {
-      
-      ball.x=paddle.x+-1*ball.dx*(paddle.h+ballSpeed);
-      
-      pickSlope();
-      
-      ball.dx*=-1;
-    }
+
+function display(){
+  fill('pink');
+  ellipse(xBall,yBall,20,20);
+}
+
+function paddle(){
+  if((xBall>mouseX &&
+     xBall<mouseX+90)&&
+    (yBall+10>=375)){
+    xSpeed*=-1;
+    ySpeed*=-1;
+    score++;
+    
+  }
 }
