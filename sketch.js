d0e360848c9fac76e957b215e0fe7b295c05a130
CCG0214 <cgu25@belmontschools.net>
2025-05-13 10:29:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,23 +4,100 @@ var xSpeed=(2,7);
 var ySpeed=(7,2);
 var score=0
 
+
 function setup(){
   createCanvas(400,400);
+  alert('use mouse to control the paddle to bounce the sun!');
 }
 
 function draw(){
   
-  background(0);
+  background('rgb(150,237,255)')
   
   fill('#FFF');
   rect(mouseX,375,90,15);
+  
+  
+  fill('rgb(255,255,255)');
+  circle(380,15,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(350,0,50);
+  noStroke();
+
+  
+  
+  fill('rgb(255,255,255)');
+  circle(50,80,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(70,60,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(30,70,40);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(90,80,50);
+  noStroke();
+  
+  
+  
+    
+  fill('rgb(255,255,255)');
+  circle(250,180,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(270,150,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(230,170,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(290,180,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(310,160,40);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(320,180,40);
+  noStroke();
+  
+  
+  fill('rgb(255,255,255)');
+  circle(50,280,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(70,260,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(30,270,40);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(90,280,50);
+  noStroke();
+  
+  fill('rgb(255,255,255)');
+  circle(110,265,50);
+  noStroke();
   
   move();
   display();
   bounce();
   paddle();
-  
-  fill('pink');
+
+  fill('#03A9F4');
   textSize(24);
   text("Score:"+score,10,25);
 }
@@ -35,15 +112,17 @@ function bounce(){
     xBall>400-10){
     xSpeed*=-1;
   }
-  if(yBall<10||
-    yBall>400-10){
+  if(yBall>400-10){
     ySpeed=-1;
+  }
+  if (yBall<10) {
+    ySpeed = 1; 
   }
 }
 
 
 function display(){
-  fill('pink');
+  fill('#FCE200');
   ellipse(xBall,yBall,20,20);
 }
 
